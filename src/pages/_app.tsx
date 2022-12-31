87deb3eb298b72977cdd2a47cc1655da5193f13c
Jem Tan <jeremytandev@gmail.com>
2023-01-01 03:28:11 +0800

    Remove dead code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
-import Dummy from '../components/dummy/dummy.component';
 
 config.autoAddCss = false;
 
